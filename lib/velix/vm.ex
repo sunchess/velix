ce24bdf675b7a5ba3c4ccce75791a59097c8afd8
Alexander Dmitriev <alexanderdmv@gmail.com>
2017-12-11 19:53:17 +0300

    added stop, remove vm

--- lib/velix/vm.ex
+++ lib/velix/vm.ex
@@ -1,19 +1,24 @@
 defmodule Velix.Vm do
   alias :verx_client, as: VerxClient
   alias :verx,        as: Verx
+  alias Velix.Client
+
   @xml "/root/vmsdump/test5.xml"
 
   def list do
-    {:ok, ref} = VerxClient.start()
-    :ok = Verx.connect_open(ref)
+    {:ok, ref} = connect()
 
-    {:ok, [num_def]} = Verx.connect_num_of_defined_domains(ref)
-    {:ok, [num_run]} = Verx.connect_num_of_domains(ref)
+    try do
+      {:ok, [num_def]} = Verx.connect_num_of_defined_domains(ref)
+      {:ok, [num_run]} = Verx.connect_num_of_domains(ref)
 
-    {:ok, [shutoff]} = Verx.connect_list_defined_domains(ref, [num_def])
-    {:ok, [running]} = Verx.connect_list_domains(ref, [num_run])
+      {:ok, [shutoff]} = Verx.connect_list_defined_domains(ref, [num_def])
+      {:ok, [running]} = Verx.connect_list_domains(ref, [num_run])
 
-    {:ok, %{running: info(ref, running), shutoff: info(ref, shutoff)}}
+      {:ok, %{running: info(ref, running), shutoff: info(ref, shutoff)}}
+    after 
+      close(ref)
+    end
   end
 
   def create_xml() do
@@ -21,39 +26,95 @@ defmodule Velix.Vm do
   end
 
   def create_xml(path) do
-    # Connect to the libvirtd socket
-    {:ok, ref} = VerxClient.start()
+    {:ok, ref} = connect()
+    try do
+      {:ok, xml} = File.read(path)
 
-    # libvirt remote procotol open message
-    :ok = Verx.connect_open(ref)
+      # Domain is defined but not running
+      {:ok, [domain]} = Verx.domain_define_xml(ref, [xml])
 
-    {:ok, xml} = File.read(path)
+      # Start the VM
+      :ok = Verx.domain_create(ref, [domain])
 
-    # Domain is defined but not running
-    {:ok, [domain]} = Verx.domain_define_xml(ref, [xml])
+      {:ok, [active]} = Verx.connect_num_of_domains(ref)
+      :io.format("Active Domains: ~p~n", [active])
 
-    # Start the VM
-    :ok = Verx.domain_create(ref, [domain])
+      {:ok, active}
+    after
+      close(ref)
+    end
+  end
 
-    {:ok, [active]} = Verx.connect_num_of_domains(ref)
-    :io.format("Active Domains: ~p~n", [active])
+  def stop(name_or_id) do
+    {:ok, ref} = connect()
+    try do
+      {:ok, domain} = lookup(ref, name_or_id)
+      :ok = stopping(ref, domain)
+      {:ok, domain}
+    after
+      close(ref)
+    end
+  end
 
-    # Send a protocol close message
-    :ok = Verx.connect_close(ref)
+  def remove(name_or_id) do
+    {:ok, ref} = connect()
 
-    # Close the socket
-    :ok = VerxClient.stop(ref)
+    try do
+      {:ok, domain} = lookup(ref, name_or_id)
+      :ok = stopping(ref, domain)
 
-    {:ok, domain}
+      Verx.domain_undefine(ref, [domain])
+    after
+      close(ref)
+    end
   end
 
 
+  # Get the domain resource
+  def lookup(ref, id) when is_integer(id) do
+    {:ok, [domain]} = Verx.domain_lookup_by_id(ref, [id])
+    {:ok, domain}
+  end
+
+  def lookup(ref, name) when is_binary(name) do
+    {:ok, [domain]} = Verx.domain_lookup_by_name(ref, [name])
+    {:ok, domain}
+  end
+
+#private
+  
+  defp running?(ref, domain) do
+    {:ok, [num_run]} = Verx.connect_num_of_domains(ref)
+    {:ok, [running]} = Verx.connect_list_domains(ref, [num_run])
+    {_, _, id} = domain
+
+    Enum.member?(running, id)
+  end
+
+  defp stopping(ref, domain) do
+    if running?(ref, domain) do
+      # shutdown only works if acpid is installed in the VM
+      :ok = Verx.domain_shutdown(ref, [domain])
+      :ok = Verx.domain_destroy(ref, [domain])
+    else
+      {name, _, _} = domain
+      IO.puts("Vm #{name} is not running")
+    end
+    :ok
+  end
 
   defp info(ref, domains) do
     for domain <- domains, into: [] do
-      {:ok, [{name, uuid, id}]} = Verx.domain_lookup_by_id(ref, [domain])
+      {:ok, {name, uuid, id}} = lookup(ref, domain)
       {name, uuid, id}
     end
   end
 
+  defp connect do
+    Client.connect()
+  end
+
+  defp close(ref) do
+    Client.close(ref)
+  end
 end
